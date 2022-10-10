3d55dc90d9786438fda906bf9ab68f40dfa28fe8
Ovidiu <61224056+CIOLAN2212@users.noreply.github.com>
2022-10-10 18:56:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+import csv
+
+#--------------------------------------------------M15--------------------------------------------------
+M15_file = csv.reader(open("AUDUSD_M15_2019_2022(10_10).csv"))
+
+M15_date = []
+M15_time = []
+M15_open = []
+M15_high = []
+M15_low = []
+M15_close = []
+
+for row in M15_file:
+    M15_date.append(row[0])
+    M15_time.append(row[1])
+    M15_open.append(row[2])
+    M15_high.append(row[3])
+    M15_low.append(row[4])
+    M15_close.append(row[5])
+
+
+M15_date = np.array(M15_date)
+M15_time = np.array(M15_time)
+M15_open = np.array(M15_open)
+M15_high = np.array(M15_high)
+M15_low = np.array(M15_low)
+M15_close = np.array(M15_close)
