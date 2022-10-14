b944c11a61082fb53ef404af6b30a0738fd19167
Ovidiu <61224056+CIOLAN2212@users.noreply.github.com>
2022-10-14 11:00:27 +0300

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -18,20 +18,27 @@ except:
 url = "https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol={0}&to_symbol={1}&outputsize=full&interval={2}&apikey={3}".format(
     SYMBOLS[0], SYMBOLS[1], TIMEFRAME, API_KEY
 )
+print(url)
 api_data = json.loads(requests.get(url).text)
 
-with open("AUDUSD_M15.csv", "a") as f:
-    f.write("\n")
+new_line = True
 for date_hour in api_data["Time Series FX (15min)"]:
-    candle = {
-        "TIME": [date_hour],
-        "OPEN": [api_data["Time Series FX (15min)"][date_hour]["1. open"]],
-        "HIGH": [api_data["Time Series FX (15min)"][date_hour]["2. high"]],
-        "LOW": [api_data["Time Series FX (15min)"][date_hour]["3. low"]],
-        "CLOSE": [api_data["Time Series FX (15min)"][date_hour]["4. close"]],
-    }
-    candle = pd.DataFrame(candle)
-    candle.to_csv("AUDUSD_M15.csv", mode="a", index=False, header=False)
+    if date_hour != first_row:
+        if new_line:
+            new_line = False
+            with open("AUDUSD_M15.csv", "a") as f:
+                f.write("\n")
+        candle = {
+            "TIME": [date_hour],
+            "OPEN": [api_data["Time Series FX (15min)"][date_hour]["1. open"]],
+            "HIGH": [api_data["Time Series FX (15min)"][date_hour]["2. high"]],
+            "LOW": [api_data["Time Series FX (15min)"][date_hour]["3. low"]],
+            "CLOSE": [api_data["Time Series FX (15min)"][date_hour]["4. close"]],
+        }
+        candle = pd.DataFrame(candle)
+        candle.to_csv("AUDUSD_M15.csv", mode="a", index=False, header=False)
+    else:
+        break
 
 # csv_file = pd.read_csv("AUDUSD_M15.csv", index_col="TIME")
 # csv_file = csv_file.sort_index(ascending=False)
